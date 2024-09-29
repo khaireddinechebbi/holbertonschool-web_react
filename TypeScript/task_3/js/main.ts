06efa498eece1fd429af07d9ed89508f2132bc49
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-09-29 12:34:38 +0200

    Ambient Namespaces

---
+++ TypeScript/task_3/js/main.ts
@@ -0,0 +1,7 @@
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
